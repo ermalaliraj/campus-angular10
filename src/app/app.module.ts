0d8c315ae559cdb372d7ddfc24ead7be2e5a427c
Ermal <ermal.aliraj@gmail.com>
2020-08-20 21:43:21 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {XyzComponent} from './xyz/xyz.component';
 import {CoreModule} from "./core/core.module";
 import {AppComponent} from "./app.component";
@@ -14,7 +14,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [
-        NgbModule,
+        // NgbModule,
         BrowserModule,
         CoreModule
         , LayoutModule
